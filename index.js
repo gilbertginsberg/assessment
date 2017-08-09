d24f748e58d404b06a07d24c47507f6e2b914ce2
Gilbert <steven.n.gilbert@gmail.com>
2017-08-09 10:29:57 -0400

    feat: Implemented removedPaddedZero function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ window.onload = () => {
   const form = document.querySelector('form');
   const number = document.querySelector('#num');
   const engPhrase = document.querySelector('#eng-phrase');
+  let unPaddedNumber = number.value;
 
   const baseNums = {
     0: 'zero',
@@ -36,8 +37,8 @@ window.onload = () => {
 
   // **Helper functions**
   function slicedNumber(digit) {
-    const indexToSliceFrom = number.value.length - digit;
-    const slicedNum = number.value.slice(indexToSliceFrom);
+    const indexToSliceFrom = unPaddedNumber.length - digit;
+    const slicedNum = unPaddedNumber.slice(indexToSliceFrom);
     return slicedNum;
   }
 
@@ -49,11 +50,11 @@ window.onload = () => {
 
    // *Covers numbers 0 to 9*
   function genLessThanTenPhrase() {
-    const lastIndex = number.value.length - 1;
-    const valueOfLastDigit = number.value[lastIndex];
+    const lastIndex = unPaddedNumber.length - 1;
+    const valueOfLastDigit = unPaddedNumber[lastIndex];
     const lessThanTenPhrase = baseNums[valueOfLastDigit];
 
-    if (number.value.length >= 3) {
+    if (unPaddedNumber.length >= 3) {
       engPhrase.textContent = `and ${lessThanTenPhrase}`;
     } else {
       engPhrase.textContent = lessThanTenPhrase;
@@ -63,15 +64,13 @@ window.onload = () => {
 
   // *Covers numbers 10 to 99*
   function genLessThan100Phrase() {
-    const secondToLastIndex = number.value.length - 2;
-    const valueOfSecondToLastDigit = number.value[secondToLastIndex];
-
-    // Phrasal chunks
+    const secondToLastIndex = unPaddedNumber.length - 2;
+    const valueOfSecondToLastDigit = unPaddedNumber[secondToLastIndex];
     const multOfTenPhrase = genMultipleOfTenPhrase(valueOfSecondToLastDigit);
     const lessThanTenPhrase = genLessThanTenPhrase();
 
-    if (number.value.length === 2 && baseNums[number.value]) {
-      engPhrase.textContent = baseNums[number.value];
+    if (unPaddedNumber.length === 2 && baseNums[unPaddedNumber]) {
+      engPhrase.textContent = baseNums[unPaddedNumber];
     } else {
       engPhrase.textContent = valueOfSecondToLastDigit === '0' ? `${lessThanTenPhrase}` : `${multOfTenPhrase}-${lessThanTenPhrase}`;
     }
@@ -82,8 +81,8 @@ window.onload = () => {
   // *Covers numbers 100 to 999*
   function gen100to999Phrase() {
     const lastTwoDigits = slicedNumber(2);
-    const thirdToLastIndex = number.value.length - 3;
-    const valueOfThirdToLastDigit = number.value[thirdToLastIndex];
+    const thirdToLastIndex = unPaddedNumber.length - 3;
+    const valueOfThirdToLastDigit = unPaddedNumber[thirdToLastIndex];
     const hundredPhrase = baseNums[valueOfThirdToLastDigit] === 'zero' ? '' : `${baseNums[valueOfThirdToLastDigit]} hundred`;
     const lessThan100Phrase = genLessThan100Phrase();
     const fullPhrase = baseNums[lastTwoDigits] ? `${hundredPhrase} ${baseNums[lastTwoDigits]}` : `${hundredPhrase} ${lessThan100Phrase}`;
@@ -96,8 +95,8 @@ window.onload = () => {
   // *Covers numbers 1000 to 9999*
   function gen1000to9999Phrase() {
     const lastThreeDigits = slicedNumber(3);
-    const fourthToLastIndex = number.value.length - 4;
-    const valueOfFourthToLastDigit = number.value[fourthToLastIndex];
+    const fourthToLastIndex = unPaddedNumber.length - 4;
+    const valueOfFourthToLastDigit = unPaddedNumber[fourthToLastIndex];
     const thousandPhrase = valueOfFourthToLastDigit === '0' ? '' : `${baseNums[valueOfFourthToLastDigit]} thousand`;
     const hundredPhrase = gen100to999Phrase();
 
@@ -109,9 +108,9 @@ window.onload = () => {
   // Covers numbers 10,000 to 99,000
   function genTenThousandsPhrase() {
     const lastFourDigits = slicedNumber(4);
-    const secondDigit = number.value[1];
-    const multOfTenPhrase = genMultipleOfTenPhrase(number.value[0]);
-    const tenThousandPhrase = secondDigit === '0' ? `${multOfTenPhrase} thousand` : `${multOfTenPhrase}-${baseNums[number.value[1]]} thousand`;
+    const secondDigit = unPaddedNumber[1];
+    const multOfTenPhrase = genMultipleOfTenPhrase(unPaddedNumber[0]);
+    const tenThousandPhrase = secondDigit === '0' ? `${multOfTenPhrase} thousand` : `${multOfTenPhrase}-${baseNums[unPaddedNumber[1]]} thousand`;
     const hundredPhrase = gen100to999Phrase();
 
     engPhrase.textContent = `${tenThousandPhrase} ${hundredPhrase}`;
@@ -128,39 +127,52 @@ window.onload = () => {
 
   }
 
-  function convertNumToEngPhrase(e) {
-    e.preventDefault();
-
-    if (number.value.length === 1) {
+  function convertNumToEngPhrase(num) {
+    if (num.length === 1) {
       return genLessThanTenPhrase();
-    } else if (number.value.length === 2) {
+    } else if (num.length === 2) {
       return genLessThan100Phrase();
-    } else if (number.value.length === 3) {
+    } else if (num.length === 3) {
       return gen100to999Phrase();
-    } else if (number.value.length === 4) {
+    } else if (num.length === 4) {
       return gen1000to9999Phrase();
-    } else if (number.value.length === 5) {
+    } else if (num.length === 5) {
       return genTenThousandsPhrase();
-    } else if (number.value.length === 6) {
-    
-    } else if (number.value.length === 7) {
-    
-    } else if (number.value.length === 8) {
-    
-    } else if (number.value.length === 9) {
-    
-    } else if (number.value.length === 10) {
-    
-    } else if (number.value.length === 11) {
-    
-    } else if (number.value.length === 12) {
-    
-    } else if (number.value.length === 13) {
-    
+    } else if (num.length === 6) {
+  
+    } else if (num.length === 7) {
+  
+    } else if (num.length === 8) {
+  
+    } else if (num.length === 9) {
+  
+    } else if (num.length=== 10) {
+  
+    } else if (num.length === 11) {
+  
+    } else if (num.length === 12) {
+  
+    } else if (num.length === 13) {
+  
     }
 
     return alert('Hola amigo o amiga. Please enter a number, 1 through 1 trillion, and you will find magic.');
   }
 
-  form.addEventListener('submit', convertNumToEngPhrase, false);
+  function removePaddedZeros(e) {
+    e.preventDefault();
+
+    const numArr = number.value.split('');
+
+    numArr.forEach((num, index) => {
+      if (num === '0') {
+        unPaddedNumber = number.value.slice(index + 1);
+      } else {
+        console.log(`unPaddedNumber is ${unPaddedNumber}`);
+        return convertNumToEngPhrase(unPaddedNumber);
+      }
+    });
+  }
+
+  form.addEventListener('submit', removePaddedZeros, false);
 };
