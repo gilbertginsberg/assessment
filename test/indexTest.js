ce6df62dc75938552751fcca1e77e3c91927d9ab
Gilbert <steven.n.gilbert@gmail.com>
2017-08-10 22:24:08 -0400

    test: Added unit tests

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,50 @@
-describe('some demo tests', () => {
-  it('adds two numbers together', () => {
-    assert(2 + 3 === 5);
+describe('Number to English Phrase Converter', () => {
+  describe('helpers', () => {
+    it('slicedNumber should slice off first digit of inputted number', () => {
+      unPaddedNumber = '12345';
+      const answer = slicedNumber(4);
+      assert.equal(answer, '2345', 'slicedNumber slices off first fist digit');
+    });
+
+    it('genMultipleOfTenPhrase should provide the phrase for a multiple of ten', () => {
+      const answer = genMultipleOfTenPhrase(9);
+      assert.equal(answer, 'ninety');
+    });
+  });
+
+  describe('conversion functions', () => {
+    it('genLessThanTenPhrase', () => {
+      const unPaddedNumber = '5'
+      const answer = genLessThanTenPhrase(unPaddedNumber);
+      assert.equal(answer, 'five');
+    });
+
+    it('genLessThan100Phrase', () => {
+      const unPaddedNumber = '45'
+    });
+
+    it('gen100to999Phrase', () => {
+     
+    });
+
+    it('genTenThousandPhrase', () => {
+     
+    });
+
+    it('gen100ThousandPhrase', () => {
+     
+    });
+
+    it('genMillionPhrase', () => {
+     
+    });
+  });
+
+  describe('The Converter', () => {
+    it('convertNumToEngPhrase should convert an arabic number to its english equivalent', () => {
+      const unPaddedNumber = '55'
+      const answer = convertNumToEngPhrase(unPaddedNumber);
+      assert.equal(answer, 'fifty-five');
+    });
   });
 });
